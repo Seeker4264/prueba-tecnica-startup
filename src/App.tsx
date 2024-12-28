d23a0e60e681074434b002dbce2df75fb8aaa4ee
Seeker4264 <sebashmorales@gmail.com>
2024-12-27 23:29:21 -0400

    color correction / corrección de colores

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ function App() {
           <p>
             {message.message_text}
           </p>
-          <span>
+          <span className="timestamp">
             <p>
               {dateDay} &nbsp;
             </p>
@@ -46,7 +46,32 @@ function App() {
       <main className="mainContainer">
         <div className="chatFrame">
           <header className="chatHeader">
-            <h1>Chat with me!</h1>
+            <div>
+              <svg xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="chatIcon">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M6 4m0 2a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v4a2 2 0 0 1 -2 2h-8a2 2 0 0 1 -2 -2z" />
+                <path d="M12 2v2" />
+                <path d="M9 12v9" />
+                <path d="M15 12v9" />
+                <path d="M5 16l4 -2" />
+                <path d="M15 14l4 2" />
+                <path d="M9 18h6" />
+                <path d="M10 8v.01" />
+                <path d="M14 8v.01" />
+              </svg>
+            </div>
+            <h1>
+              Chat with me!
+            </h1>
           </header>
           <div className="chatMain">
             {
